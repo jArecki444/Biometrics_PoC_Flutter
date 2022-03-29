e652dd4f6ae34dd3b8f1c3acc1b7c50164721582
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2022-03-29 13:41:42 +0200

    Simple authentication based on available biometrics

---
+++ lib/presentation/auth_page.dart
@@ -0,0 +1,71 @@
+import 'package:biometrics_auth_poc/api/local_auth_api.dart';
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  List<BiometricType> _availableBiometrics = [];
+  bool? _canCheckBiometrics;
+  bool _authorized = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Biometrics PoC'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Visibility(
+              visible: _canCheckBiometrics ?? false,
+              child: Text('Can check biometrics: $_canCheckBiometrics'),
+            ),
+            ElevatedButton(
+              child: const Text('Check biometrics'),
+              onPressed: () async {
+                final hasBiometrics = await LocalAuthApi.hasBiometrics();
+                setState(() {
+                  _canCheckBiometrics = hasBiometrics;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            Text('Authorized: $_authorized'),
+            ElevatedButton(
+              child: const Text('Authenticate'),
+              onPressed: () async {
+                final isAuthenticated = await LocalAuthApi.authenticate();
+                setState(() {
+                  _authorized = isAuthenticated;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text('Available biometrics: $_availableBiometrics'),
+            ElevatedButton(
+              child: const Text('Get available biometrics'),
+              onPressed: () async {
+                final biometrics = await LocalAuthApi.getAvailableBiometrics();
+                setState(() {
+                  _availableBiometrics = biometrics;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
