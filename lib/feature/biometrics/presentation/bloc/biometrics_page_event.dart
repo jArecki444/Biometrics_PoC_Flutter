9fb7193cdd63279f59af328fc7746fcc77d58ac4
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2022-04-05 14:46:28 +0200

    Add missing comments to biometrics page events and state

--- lib/feature/biometrics/presentation/bloc/biometrics_page_event.dart
+++ lib/feature/biometrics/presentation/bloc/biometrics_page_event.dart
@@ -2,9 +2,15 @@ part of 'biometrics_page_bloc.dart';
 
 @freezed
 class BiometricsPageEvent with _$BiometricsPageEvent {
+
+  ///This event is used after bloc initialization, to determine available biometrics options.
   const factory BiometricsPageEvent.setBiometricsAvailability() =
       SetBiometricsAvailabilityEvent;
+
+  ///This event is used when user wants to authorize with biometrics.
   const factory BiometricsPageEvent.tryToAuthorizeWithBiometrics() =
       TryToAuthorizeWithBiometricsEvent;
+
+  ///This event is used when user wants to restart authentication process.
   const factory BiometricsPageEvent.restartAuthState() = RestartAuthStateEvent;
 }
--- lib/feature/biometrics/presentation/bloc/biometrics_page_state.dart
+++ lib/feature/biometrics/presentation/bloc/biometrics_page_state.dart
@@ -10,14 +10,27 @@ class BiometricsPageState with _$BiometricsPageState {
 
 @freezed
 class PageStatus with _$PageStatus {
+  /// This status will be used when there is at least one biometrics option available.
+  /// It is a one of possible results after emitting setBiometricsAvailability event.
   const factory PageStatus.waitingForSelectionOfAuthMethod() =
       _WaitingForSelectionOfAuthMethod;
+
+  /// This status is initial, when user enters biometrics page.
+  /// During this status we will check if there is at least one biometrics option available.
+  /// This status will be established after emitting setBiometricsAvailability event.
   const factory PageStatus.checkingBiometricsAvailability() =
       _CheckingBiometricsAvailability;
+
+  /// This status will be set after successful biometrics authentication.
   const factory PageStatus.authorized() = _Authorized;
+
+  /// This status will be set after unsuccessful biometrics authentication.
   const factory PageStatus.unauthorized() = _Unauthorized;
+
+  /// This status will be set when biometrics is unavailable.
+  /// It is a one of possible results after emitting setBiometricsAvailability event.
+  /// We need to provide reason of unavailable biometrics, so we can show appropriate message to user.
   const factory PageStatus.biometricsUnavailable({
     required UnavailableBiometricsReason reason,
   }) = _BiometricsUnavailable;
 }
-
